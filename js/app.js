d59dd94e1b99306aaac3e3c8014963e802febbde
Daniel Zegerson <danizeger283@gmail.com>
2022-07-13 12:28:56 +0300

    #

--- js/app.js
+++ js/app.js
@@ -8,12 +8,18 @@ function addItem() {
     bookItem.value = "";
 }
 
+function mark(listItem) {
+  
+  listItem.style.textDecoration = "line-through";
+}
+
+
 function dispList() {
     let disItem = document.getElementById("disp");
     console.log(disItem);
     disItem.innerHTML = '<ul>';
     for (bookList of list){
-        disItem.innerHTML += '<li id="'+bookList+'" onclick="mark('+bookList+')">' + bookList + '</li>'
+        disItem.innerHTML += '<li id="'+bookList+'" onClick="mark('+bookList+')">' + bookList + '</li>'
     }
     disItem.innerHTML += '</ul>';
 }
